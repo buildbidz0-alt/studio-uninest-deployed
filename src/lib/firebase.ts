9047571cf299448ed7a40880e5e3bc30ef9ba696
Buildbidz <buildbidz0@gmail.com>
2025-09-08 04:54:14 +0000

    login system is still not functional

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  "apiKey": "YOUR_API_KEY",
-  "authDomain": "YOUR_AUTH_DOMAIN",
-  "projectId": "YOUR_PROJECT_ID",
-  "storageBucket": "YOUR_STORAGE_BUCKET",
-  "messagingSenderId": "YOUR_MESSAGING_SENDER_ID",
-  "appId": "YOUR_APP_ID"
+  "apiKey": "FIREBASE_API_KEY",
+  "authDomain": "FIREBASE_AUTH_DOMAIN",
+  "projectId": "FIREBASE_PROJECT_ID",
+  "storageBucket": "FIREBASE_STORAGE_BUCKET",
+  "messagingSenderId": "FIREBASE_MESSAGING_SENDER_ID",
+  "appId": "FIREBASE_APP_ID"
 };
 
 // Initialize Firebase
