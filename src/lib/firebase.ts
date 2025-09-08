af0abe03cfc48381db50a47899bfa51c961c3f00
Buildbidz <buildbidz0@gmail.com>
2025-09-08 05:18:09 +0000

    solv it (_for element <Primitive.div>_)

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  "apiKey": "FIREBASE_API_KEY",
-  "authDomain": "FIREBASE_AUTH_DOMAIN",
-  "projectId": "FIREBASE_PROJECT_ID",
-  "storageBucket": "FIREBASE_STORAGE_BUCKET",
-  "messagingSenderId": "FIREBASE_MESSAGING_SENDER_ID",
-  "appId": "FIREBASE_APP_ID"
+  "apiKey": "STUDIO_FIREBASE_API_KEY",
+  "authDomain": "STUDIO_FIREBASE_AUTH_DOMAIN",
+  "projectId": "STUDIO_FIREBASE_PROJECT_ID",
+  "storageBucket": "STUDIO_FIREBASE_STORAGE_BUCKET",
+  "messagingSenderId": "STUDIO_FIREBASE_MESSAGING_SENDER_ID",
+  "appId": "STUDIO_FIREBASE_APP_ID"
 };
 
 // Initialize Firebase
